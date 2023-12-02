01d24d42a2e0593239b01ff0457b8605827ceb05
Victor Matia <vmatir@gmail.com>
2023-12-02 07:28:38 +0000

    Added the calling conventions.
    Added the supported calling conventions to the imports.

--- Gnome.Net.GLib/src/Imports/GArrayImports.cs
+++ Gnome.Net.GLib/src/Imports/GArrayImports.cs
@@ -1,6 +1,7 @@
 // This file is part of the Gnome.Net project and is under the MIT license.
 // See LICENSE.md for more information.
 
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Gnome.Net.GLib.Imports;
@@ -8,5 +9,6 @@ namespace Gnome.Net.GLib.Imports;
 internal static partial class GArrayImports
 {
     [LibraryImport("GLib", EntryPoint = "g_array_unref")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial void GArrayUnref(nint array);
 }
