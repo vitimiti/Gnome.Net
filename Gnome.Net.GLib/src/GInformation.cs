b3a01e5baeced053b56e6970c3bb349964aed64f
Victor Matia <vmatir@gmail.com>
2023-12-03 06:18:45 +0000

    Added an information class.
    Added the application name information.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Gnome.Net.GLib/src/GInformation.cs
@@ -0,0 +1,58 @@
+// This file is part of the Gnome.Net project and is under the MIT license.
+// See LICENSE.md for more information.
+
+using Gnome.Net.GLib.Imports;
+
+namespace Gnome.Net.GLib;
+
+/// <summary>Information related functions and properties.</summary>
+public static class GInformation
+{
+    /// <summary>Gets/Sets a human-readable name for the application.</summary>
+    /// <value>
+    ///     A <see cref="string" /> with the application name. This may return <see langword="null" /> but cannot be
+    ///     given a <see langword="null" /> <see langword="value" />.
+    /// </value>
+    /// <exception cref="ArgumentNullException">
+    ///     If the <see langword="value" /> is <see langword="null" /> when setting the application name.
+    /// </exception>
+    /// <remarks>
+    ///     This name should be localized if possible, and is intended for display to the user. Contrast with
+    ///     <see cref="ProgramName" />, which gets a non-localized name. If the application name hasn't been set, the
+    ///     getter returns the value of <see cref="ProgramName" />.
+    /// </remarks>
+    public static string? ApplicationName
+    {
+        get => GLibImports.GStringDuplicate(GLibImports.GGetApplicationName());
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            GLibImports.GSetApplicationName(value);
+        }
+    }
+
+    // TODO: Ensure the non dependent libraries documentation is correctly named once implemented.
+    /// <summary>Gets the name of the program.</summary>
+    /// <value>
+    ///     A <see cref="string" /> with the program name. This may return <see langword="null" /> but cannot be
+    ///     given a <see langword="null" /> <see langword="value" />.
+    /// </value>
+    /// <exception cref="ArgumentNullException">
+    ///     If the <see langword="value" /> is <see langword="null" /> when setting the program name.
+    /// </exception>
+    /// <remarks>
+    ///     This name should not be localized, in contrast to <see cref="ApplicationName" />. If you are using
+    ///     GApplication the program name is set in GApplication.Run(). In case of GDK or GTK+ it is set in
+    ///     Gdk.GEngine.Init(), which is called by Gtk.GEngine.Init(), and the GtkApplication.Startup handler. The
+    ///     program name is found by taking the last component of the application arguments.
+    /// </remarks>
+    public static string? ProgramName
+    {
+        get => GLibImports.GStringDuplicate(GLibImports.GGetProgramName());
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            GLibImports.GSetProgramName(value);
+        }
+    }
+}
--- Gnome.Net.GLib/src/Imports/GLibImports.cs
+++ Gnome.Net.GLib/src/Imports/GLibImports.cs
@@ -11,4 +11,39 @@ internal static partial class GLibImports
 {
     [LibraryImport(LibraryName.Default, EntryPoint = "g_free")]
     public static partial void GFree(nint memory);
+
+    [LibraryImport(
+        LibraryName.Default,
+        EntryPoint = "g_get_application_name",
+        StringMarshalling = StringMarshalling.Utf8
+    )]
+    public static partial nint GGetApplicationName();
+
+    [LibraryImport(
+        LibraryName.Default,
+        EntryPoint = "g_get_prgname",
+        StringMarshalling = StringMarshalling.Utf8
+    )]
+    public static partial nint GGetProgramName();
+
+    [LibraryImport(
+        LibraryName.Default,
+        EntryPoint = "g_set_application_name",
+        StringMarshalling = StringMarshalling.Utf8
+    )]
+    public static partial void GSetApplicationName(string applicationName);
+
+    [LibraryImport(
+        LibraryName.Default,
+        EntryPoint = "g_set_prgname",
+        StringMarshalling = StringMarshalling.Utf8
+    )]
+    public static partial void GSetProgramName(string applicationName);
+
+    [LibraryImport(
+        LibraryName.Default,
+        EntryPoint = "g_strdup",
+        StringMarshalling = StringMarshalling.Utf8
+    )]
+    public static partial string? GStringDuplicate(nint strPtr);
 }
