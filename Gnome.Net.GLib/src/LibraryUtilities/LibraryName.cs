6f807478ca5ceb30717b11492c1545980e952dea
Victor Matia <vmatir@gmail.com>
2023-12-03 16:58:49 +0000

    Added missing library name.
    Now the library name is included for use.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Gnome.Net.GLib/src/LibraryUtilities/LibraryName.cs
@@ -0,0 +1,13 @@
+// This file is part of the Gnome.Net project and is under the MIT license.
+// See LICENSE.md for more information.
+
+namespace Gnome.Net.GLib.LibraryUtilities;
+
+internal struct LibraryName
+{
+    internal const string WindowsName = "libglib-2.0.0.dll";
+    internal const string OsxName = "libglib-2.0.0.dylib";
+    internal const string UnixName = "libglib-2.0.so.0";
+
+    public const string GLib = "GLib";
+}
